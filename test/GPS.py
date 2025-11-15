58bff9dc46028931edf623f9b647516f0d06e994
iori3662 <7522111@ed.tus.ac.jp>
2025-11-15 14:45:06 +0900

    Add:GPSサンプル

---
+++ test/GPS.py
@@ -0,0 +1,41 @@
+# ライブラリ：Melopero_SAM-M8Q
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Leonardo La Rocca
+"""
+
+import melopero_samm8q as mp
+import melopero_ubx as ubx
+import time
+
+gps = mp.SAM_M8Q()
+
+#Set the communication protocol to ubx for input and output
+gps.ubx_only()
+gps.wait_for_acknowledge(ubx.CFG_CLASS, ubx.CFG_PRT)
+
+#Set the navigation message to be sent every time there is a navigation solution
+gps.set_message_frequency(ubx.NAV_CLASS, ubx.NAV_PVT, 1)
+gps.wait_for_acknowledge(ubx.CFG_CLASS ,ubx.CFG_MSG)
+
+#Set the measurement frequency to 50 ms and send a navigation solution every second measurement
+# this results in a navigation solution every 100ms 
+gps.set_measurement_frequency(50, 2)
+gps.wait_for_acknowledge(ubx.CFG_CLASS, ubx.CFG_RATE)
+
+#take a measurement every 0.1 seconds for an hour 
+for i in range(36000):
+    info = gps.get_pvt()
+    
+    #if there is a valid measurement
+    if info:
+        print("Valid time : {}, Valid date : {}".format(info[gps.VALID_TIME_TAG], info[gps.VALID_DATE_TAG]))
+        print("[{}/{}/{}] {}h:{}m:{}s".format(info[gps.DAY_TAG], info[gps.MONTH_TAG], info[gps.YEAR_TAG],
+              info[gps.HOUR_TAG], info[gps.MINUTE_TAG], info[gps.SECOND_TAG]))
+        print("GNSS Fix: {}".format(info[gps.GNSS_FIX_TAG]))
+        print("Coordinates: {} N {} E".format(info[gps.LATITUDE_TAG], info[gps.LONGITUDE_TAG]))
+    
+    else :
+        print("Something went wrong and no measurement was taken")
